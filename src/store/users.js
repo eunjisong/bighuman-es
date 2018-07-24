1a9622b072570c5341e72f4424b256ef08781d50
Eunji Song <eunji.l.song@gmail.com>
2018-07-23 21:56:19 -0400

    reducer completed

--- src/store/users.js
+++ src/store/users.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const serverUrl = 'https://bh-interview.now.sh';
+const server = 'https://bh-interview.now.sh';
 
 
 
@@ -12,6 +12,41 @@ const getUser = users => ({type: GET_USER, users});
 const newPost = post => ({type: NEW_POST, post});
 const clearPosts = posts => ({type: CLEAR_POSTS, posts});
 
-// export const fetchUsers => () =>
-//     dispatch =>
 
+// **GET /users**
+export const fetchUsers = () =>
+    dispatch =>
+      axios.get(`${server}/users`)
+        .then( res => dispatch(getUser(res.data)))
+        .catch( err => console.error('error fetchingUsers', err))
+
+
+// **POST /users/:userId/posts**
+export const postNewPost = (userId, newPost) =>
+    dispatch =>
+      axios.post(`${server}/users/${userId}/posts`, newPost)
+        .then(res => dispatch(newPost(res.data)))
+        .catch( err => console.error('error posting a new post', err))
+
+
+// **GET /users/posts/clear**
+export const emptyPosts = () =>
+    dispatch =>
+      axios.get(`${server}/users/posts/clear`)
+        .then( res => dispatch(clearPosts(res.data)))
+        .catch( err => console.error('error clearing all posts for all users', err))
+
+
+
+export default ( state=[], action) => {
+    switch(action.type) {
+      case GET_USER:
+        return action.users;
+      case NEW_POST:
+        return [...state, action.post];
+      case CLEAR_POSTS:
+        return state.map( user => users.posts = []);
+      default:
+       return state;
+    }
+}
